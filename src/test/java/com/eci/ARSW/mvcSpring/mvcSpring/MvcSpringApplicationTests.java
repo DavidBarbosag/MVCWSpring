9222b8891fe15a509db18ceddeea849592e26eed
unknown <david.barbosa-g@labinfo.is.escuelaing.edu.co>
2025-06-24 12:03:40 -0500

    Commit inicial

---
+++ src/test/java/com/eci/ARSW/mvcSpring/mvcSpring/MvcSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eci.ARSW.mvcSpring.mvcSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
