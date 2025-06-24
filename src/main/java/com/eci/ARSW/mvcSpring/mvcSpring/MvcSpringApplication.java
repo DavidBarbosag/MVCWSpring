cb6e301ca63faa1e319e55827bc08fa187f46f46
unknown <david.barbosa-g@labinfo.is.escuelaing.edu.co>
2025-06-24 12:57:29 -0500

    iniciando otra tarea

--- src/main/java/com/eci/ARSW/mvcSpring/mvcSpring/MvcSpringApplication.java
+++ src/main/java/com/eci/ARSW/mvcSpring/mvcSpring/MvcSpringApplication.java
@@ -2,6 +2,11 @@ package com.eci.ARSW.mvcSpring.mvcSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MvcSpringApplication {
@@ -10,4 +15,21 @@ public class MvcSpringApplication {
 		SpringApplication.run(MvcSpringApplication.class, args);
 	}
 
+	@RequestMapping(
+			value = "/status",
+			method = RequestMethod.GET,
+			produces = "application/json"
+	)
+	public String status() {
+		return "{\"status\":\"Greetings from Spring Boot. "
+				+ java.time.LocalDate.now() + ", "
+				+ java.time.LocalTime.now()
+				+ ". " + "The server is Runnig!\"}";
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World")
+						String name) {
+		return String.format("Hello %s!", name);
+	}
 }
+
